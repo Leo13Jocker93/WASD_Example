4904de7a12424fb1724737506b32b08b72d8dbba
Leo13Jocker93 <leoroot13@gmail.com>
2025-05-21 11:35:30 -0500

    correccion de las capas de animación

---
+++ Assets/Scripts/Escenario/DestroyManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DestroyManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Destroy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
